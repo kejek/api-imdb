f88791950c0c14191f09248b465559e973acb18a
Matt Goldsworthy <matt.j.goldsworthy@gmail.com>
2023-10-24 09:47:04 -0600

    Use login page as home

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    });
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
